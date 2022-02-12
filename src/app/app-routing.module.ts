e1715a94493dce3230b489e8bf664786092e5f98
Sergio Garcia <sergio.gl86@gmail.com>
2022-02-12 10:27:12 +0100

    Se añade navbar + navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,27 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [{ path: 'instaface-home', loadChildren: () => import('./pages/instaface-home/instaface-home.module').then(m => m.InstafaceHomeModule) }, { path: 'instaface-user', loadChildren: () => import('./pages/instaface-user/instaface-user.module').then(m => m.InstafaceUserModule) }];
+const routes: Routes = [
+  {
+    path: "instaface-home",
+    loadChildren: () =>
+      import("./pages/instaface-home/instaface-home.module").then(
+        (m) => m.InstafaceHomeModule
+      ),
+  },
+  {
+    path: "instaface-user",
+    loadChildren: () =>
+      import("./pages/instaface-user/instaface-user.module").then(
+        (m) => m.InstafaceUserModule
+      ),
+  },
+  {
+    path: "**",
+    redirectTo: "/instaface-home",
+    pathMatch: "full",
+  },
+];
 
 @NgModule({
   declarations: [],
