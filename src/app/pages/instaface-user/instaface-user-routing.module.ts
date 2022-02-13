186ccc69fa1e4408997eaa0369231d8fa02171b4
Sergio Garcia <sergio.gl86@gmail.com>
2022-02-13 11:38:08 +0100

    Se implementa el metodo para crear publicación

--- src/app/pages/instaface-user/instaface-user-routing.module.ts
+++ src/app/pages/instaface-user/instaface-user-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { InstafaceUserComponent } from './instaface-user.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { InstafaceUserComponent } from "./instaface-user.component";
+import { UserListComponent } from "../../components/user-list/user-list.component";
+import { CreatePictureComponent } from "../../components/create-picture/create-picture.component";
 
-const routes: Routes = [{ path: '', component: InstafaceUserComponent }];
+const routes: Routes = [
+  {
+    path: "",
+    component: InstafaceUserComponent,
+    children: [
+      { path: "pictures", component: UserListComponent },
+      { path: "create-picture", component: CreatePictureComponent },
+      {
+        path: "**",
+        redirectTo: "pictures",
+        pathMatch: "full",
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class InstafaceUserRoutingModule { }
+export class InstafaceUserRoutingModule {}
